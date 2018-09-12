7b3c3339bbbf69be3954c98b6274579b9f41fd54
pilojo <pilo0158@algonquinlive.com>
2018-09-12 14:11:28 -0400

    Committing

---
+++ src/my_first_project.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl;
+	cout << "@author John Pilon(pilo0158@algonquinlive.com)" << endl;
+	cout << "I used to contribute to a game called The Powder Toy(https://github.com/ThePowderToy/The-Powder-Toy)" << endl;
+	return 0;
+}
